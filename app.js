ef93ca701fe17f7e5806b96c4d6797484744d525
Olushola Joshua <olusholajoshua7@gmail.com>
2023-12-28 00:42:01 -0600

    commit: 5

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-app.use(morgan("combined", { stream: accessLogStream }));
+app.use(morgan("combined"));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
